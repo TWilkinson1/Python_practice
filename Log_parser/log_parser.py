3c078f4770918afbb7ed4322539e14b1cf8557cc
TWilkinson1 <tomwilki98@googlemail.com>
2025-06-15 15:28:02 +0100

    initial functions

--- Log_parser/log_parser.py
+++ Log_parser/log_parser.py
@@ -3,4 +3,9 @@ from collections import Counter
 from datetime import datetime
 
 LOG_PATTERN = re.compile(r'(?P<ip>\S+) \S+ \[(?P<timestams>[^\]]+)\]" (?P<method>\S+) (?P<url>\S+) \S+" (?P<status>\d{3}) (?P<size>\d+)')
-                         
+
+def parse_log_line(line):
+
+def read_log_file():
+
+def summarise_log()
