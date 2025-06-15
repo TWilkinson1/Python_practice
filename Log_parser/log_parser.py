bd1abafa3124b2237669b5deb31e9512b6de6873
TWilkinson1 <tomwilki98@googlemail.com>
2025-06-15 15:42:59 +0100

    add prints to check script and fix typos

--- Log_parser/log_parser.py
+++ Log_parser/log_parser.py
@@ -1,26 +1,30 @@
 import re
-
 from collections import Counter
 from datetime import datetime
 
-LOG_PATTERN = re.compile(r'(?P<ip>\S+) \S+ \[(?P<timestams>[^\]]+)\]" (?P<method>\S+) (?P<url>\S+) \S+" (?P<status>\d{3}) (?P<size>\d+)')
+LOG_PATTERN = re.compile(r'(?P<ip>\S+) \S+ \[(?P<timestamp>[^\]]+)\]" (?P<method>\S+) (?P<url>\S+) \S+" (?P<status>\d{3}) (?P<size>\d+)')
 
 def parse_log_line(line):
     match = LOG_PATTERN.match(line)
     if match:
         data = match.groupdict()
-        data['timestamp'] = datetime.strptime(data['timestams'], '%d/%b/%Y:%H:%M:%S %z')
+        data['timestamp'] = datetime.strptime(data['timestamp'], '%d/%b/%Y:%H:%M:%S %z')
         return data
     return None
 
+
 def read_log_file(filepath):
     with open(filepath, 'r') as f: 
+        print(filepath)
         return [parse_log_line(line) for line in f if parse_log_line(line) is not None]
+    
 
 
 def summarise_log(logs):
     ip_counter = Counter(log['ip'] for log in logs)
+    print(logs)
     status_counter = Counter(log['status'] for log in logs)
+    
 
     print("Top 5 IP addresses:")
     for ip, count in ip_counter.most_common(5):
