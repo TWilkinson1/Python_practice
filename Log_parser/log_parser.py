38735bcae8461b9933fd4af828706b78ebc6454b
TWilkinson1 <tomwilki98@googlemail.com>
2025-06-15 15:29:51 +0100

    1st function

--- Log_parser/log_parser.py
+++ Log_parser/log_parser.py
@@ -5,6 +5,12 @@ from datetime import datetime
 LOG_PATTERN = re.compile(r'(?P<ip>\S+) \S+ \[(?P<timestams>[^\]]+)\]" (?P<method>\S+) (?P<url>\S+) \S+" (?P<status>\d{3}) (?P<size>\d+)')
 
 def parse_log_line(line):
+    match = LOG_PATTERN.match(line)
+    if match:
+        data = match.groupdict()
+        data['timestamp'] = datetime.strptime(data['timestams'], '%d/%b/%Y:%H:%M:%S %z')
+        return data
+    return None
 
 def read_log_file():
 
